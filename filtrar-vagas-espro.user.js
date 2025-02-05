3e5932493687e6ff35cbb41db4fee0fc681384d7
Lucas Camilo Carvalho <109776371+luascfl@users.noreply.github.com>
2025-02-05 04:33:38 -0300

    Add files via upload

--- filtrar-vagas-espro.user.js
+++ filtrar-vagas-espro.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Filtrar Vagas - Espro
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Oculta vagas que não estão no seu perfil (com círculo vermelho) e remove vagas de São Paulo (SP) ou Lauro de Freitas (BA).
 // @home         https://github.com/luascfl/filtrar-vagas-espro
 // @supportURL   https://github.com/luascfl/filtrar-vagas-espro/issues
 // @updateURL    https://raw.githubusercontent.com/luascfl/filtrar-vagas-espro/master/filtrar-vagas-espro.user.js
 // @downloadURL  https://raw.githubusercontent.com/luascfl/filtrar-vagas-espro/master/filtrar-vagas-espro.user.js
-// @author       Ibit - The Best Torrents
+// @author       luascfl
 // @license      MIT License
 // @icon         https://raw.githubusercontent.com/luascfl/filtrar-vagas-espro/master/logo.png
 // @author       luascfl
